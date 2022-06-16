aae2add35ef102777eff025e66bd7d00030563de
Utschie <littlecat.j@hotmail.com>
2022-06-16 22:47:20 +0200

    restart with ros2-rolling on ubuntu 22.04

--- build/simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jsy/Fliesenleger/simulation
-# Build directory: /home/jsy/Fliesenleger/build/simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
