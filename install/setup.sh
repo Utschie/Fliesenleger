12d20079f2700732c8c8f227704d2cf384bab7bf
Utschie <littlecat.j@hotmail.com>
2022-06-16 22:36:12 +0200

    20220616

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/jsy/Fliesenleger/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/jsy/Dropbox/Fliesenleger/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
@@ -21,7 +21,7 @@ fi
 _colcon_prefix_chain_sh_source_script() {
   if [ -f "$1" ]; then
     if [ -n "$COLCON_TRACE" ]; then
-      echo ". \"$1\""
+      echo "# . \"$1\""
     fi
     . "$1"
   else
@@ -31,7 +31,7 @@ _colcon_prefix_chain_sh_source_script() {
 
 # source chained prefixes
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/opt/ros/foxy"
+COLCON_CURRENT_PREFIX="/opt/ros/rolling"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 
